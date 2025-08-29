23d44d8d838b6bc48244eb97c9ba572ca2328b43
abhishekkumar3333 <shikhawebtsep@gmail.com>
2025-08-29 13:20:04 +0530

    file deleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react'
 import Todo from './Todo/Todo';
-import SecondTodo from './Todo/SecondTodo';
 
 const App = () => {
   return (
   <>
   <Todo/>
-  <SecondTodo/>
   </>
   )
 }
