8ea75a3323fc628c843cdbe570fa24f2ba90968e
abhishekkumar3333 <abhishekatter3@gmail.com>
2026-05-23 10:08:58 +0530

    chnages

--- backend/config/db.js
+++ backend/config/db.js
@@ -12,3 +12,8 @@ const connectDB = async () => {
 };
 
 export default connectDB;
+
+
+// PORT=5000
+// MONGO_URI=mongodb://abhishekatter3_db_user:vK1S99vd5SRaCW00@ac-ekvcjtk-shard-00-00.sezhlqm.mongodb.net:27017,ac-ekvcjtk-shard-00-01.sezhlqm.mongodb.net:27017,ac-ekvcjtk-shard-00-02.sezhlqm.mongodb.net:27017/?ssl=true&replicaSet=atlas-ybuxxo-shard-0&authSource=admin&appName=crud
+// JWT_SECRET=your_secret_key_change_in_production
